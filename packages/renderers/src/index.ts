b1d94b05faa9d667c5622cc85f635fcb6652e310
ERoydev <e.roydev@gmail.com>
2025-09-04 09:17:42 +0300

    add:() Dart renderer added to renderers index

--- packages/renderers/src/index.ts
+++ packages/renderers/src/index.ts
@@ -1,6 +1,10 @@
 import { renderVisitor as renderersJs } from '@codama/renderers-js';
 import { renderVisitor as renderersJsUmi } from '@codama/renderers-js-umi';
 import { renderVisitor as renderersRust } from '@codama/renderers-rust';
+import { renderVisitor as renderersDart } from '@codama/renderers-dart';
+
+/** @deprecated Use `renderVisitor` from `@codama/renderers-dart` instead. */
+export const renderDartVisitor = renderersDart;
 
 /** @deprecated Use `renderVisitor` from `@codama/renderers-js` instead. */
 export const renderJavaScriptVisitor = renderersJs;
